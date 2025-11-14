90417ab659fdedb7a6fcb59de0df8069b3c6acd3
VonRiddarn <vonriddarn@gmail.com>
2025-11-14 02:06:03 +0100

    docs: add file documentation and improve comments

--- javascript/routing.js
+++ javascript/routing.js
@@ -1,7 +1,12 @@
+/* 
+	This file handles the global routing state.
+	How it works is that it simply calculates the current route using a tokenized URL each refresh.
+*/
+
 // Tokenize the URL into an array of ["https:", ... "index.html"]
 export const urlTokens = document.URL.split("/");
 
-// Take the last element and tokenize it into name, type and extract the name (route). ["index", "html"]
+// Take the last element and tokenize it into [name, type] and extract the name (route). ["index", "html"]
 export const currentRoute = urlTokens[urlTokens.length - 1].split(".")[0];
 
 export const isRoute = (route) => {
