d59537c684fbfce72ec0b2159332e0d15eda9274
VonRiddarn <vonriddarn@gmail.com>
2025-11-18 10:32:53 +0100

    feat: connect button identifiers to actions

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,14 @@
 import "./nav-injector.js"; // Runs the injector script and makes sure we have a nav bar.
 console.log("Hello world!");
+
+document.getElementById("call-to-action-btn")?.addEventListener("click", (e) => {
+	window.location.href = "../pages/pricing.html";
+});
+
+const cards = document.getElementsByClassName("pricing-card");
+
+for (const card of cards) {
+	card.addEventListener("click", (e) => {
+		window.alert("Your credit card has been drained. Thank you for your purchase.");
+	});
+}
